8cb64846704b9a4b2f550be649f109424945203d
Iron_E <code.iron.e@gmail.com>
2020-10-25 13:37:36 -0400

    Add setup instructions

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ mv "lua/$1.lua" "lua/$2.lua"
 
 sed "s/$1/$2/g" "colors/$1.vim" > "colors/$2.vim"
 rm "colors/$1.vim"
+
+echo "Run 'set rtp+=$(pwd)' from within Vim to include this folder in your runtime path."
+echo ""
+echo "You must do this before sourcing your colorscheme."
