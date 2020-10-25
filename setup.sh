187786ecbd88e4a1291b25a497a3771def0d32a4
benjaminjamesxyz <71820260+benjaminjamesxyz@users.noreply.github.com>
2020-10-25 10:10:26 +0530

    added instructions and moving file to directory
    Some instruction for the user to do after executing the setup.sh was added and the files were moved toward its corresponding plugin directory.

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@ mv "lua/$1.lua" "lua/$2.lua"
 
 sed "s/$1/$2/g" "colors/$1.vim" > "colors/$2.vim"
 rm "colors/$1.vim"
+
+mkdir ../$2
+mv * ../$2
+
+echo "Put the line shown below in init.vim to load your plugin"
+echo "call plug#begin(<your plugin manager directorty>)"
+echo "Plug '$(pwd)'"
+echo "call plug#end()"
