d0bdf0cb2d11064ac09d57bcfd48b53f0ee77124
Iron-E <code.iron.e@gmail.com>
2021-06-14 14:02:54 -0400

    fix(setup): `sed` documentation
    These files escaped being parsed in the setup script, when they really
    should have been.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/sh
+sed "s/$1/$2/g" "colors/$1.lua" > "colors/$2.lua"
+rm "colors/$1.lua"
+
+sed "s/$1/$2/g" "doc/$1.txt" > "doc/$2.txt"
+rm "doc/$1.txt"
+
 mv "lua/$1.lua" "lua/$2.lua"
 
-sed "s/$1/$2/g" "colors/$1.vim" > "colors/$2.vim"
-rm "colors/$1.vim"
+sed "s/$1/$2/g" "README.md" > "README_2.md"
+mv "README_2.md" "README.md"
 
 echo "Run 'set rtp+=$(pwd)' from within Vim to include this folder in your runtime path."
 echo ""
-echo "You must do this before sourcing your colorscheme."
+echo "You must do this before sourcing your colorscheme, unless you are using a plugin manager."
