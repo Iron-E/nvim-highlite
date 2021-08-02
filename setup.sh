b89b9be1e40c4ffd042e31f7660936e59e068ffb
Iron-E <code.iron.e@gmail.com>
2021-08-02 19:01:41 -0400

    feat(setup): `upstream` remote when running script
    This is so that users can keep track of changes to the plugin while
    maintaining their own changes.
    
    CLOSES #14

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+
 sed "s/$1/$2/g" "colors/$1.lua" > "colors/$2.lua"
 rm "colors/$1.lua"
 
@@ -9,6 +10,9 @@ mv "lua/$1.lua" "lua/$2.lua"
 
 sed "s/$1/$2/g" "README.md" > "README_2.md"
 mv "README_2.md" "README.md"
+
+git remote add upstream https://github.com/Iron-E/nvim-highlite.git
+git fetch --all
 
 echo "Run 'set rtp+=$(pwd)' from within Vim to include this folder in your runtime path."
 echo ""
