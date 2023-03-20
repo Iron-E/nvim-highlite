26a272937f1f3ff5c38d67662c5d7cc76148a4a5
Iron-E <code.iron.e@gmail.com>
2023-03-20 12:30:21 -0400

    fix(update): checkout correct branch

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 git fetch --all
-git checkout upstream/master lua/highlite/init.lua
+git checkout upstream/master-v3 lua/highlite/init.lua
 mv lua/highlite/init.lua $(find lua -type f -name 'init.lua' -not -path 'lua/highlite/*')
