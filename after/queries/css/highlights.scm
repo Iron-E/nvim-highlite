ca51fa142c24b64437b0e3478022ff5819c48ce4
Iron-E <code.iron.e@gmail.com>
2023-05-02 23:44:19 -0400

    feat: add better TS css support

---
+++ after/queries/css/highlights.scm
@@ -0,0 +1,7 @@
+;; extends
+
+(class_name) @lsp.type.class
+(id_name) @label
+((plain_value) @macro (#lua-match? @macro "^%-"))
+((property_name) @preproc (#lua-match? @preproc "^%-"))
+(tag_name) @type.builtin
