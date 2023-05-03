176303307729e45fb6a106a65c004c11ee390841
Iron-E <code.iron.e@gmail.com>
2023-05-03 01:03:10 -0400

    perf(ftplugin): improve lazy loading capability

--- ftplugin/rust/highlite.lua
+++ ftplugin/rust/highlite.lua
@@ -1,5 +1,3 @@
-local Treesitter = require 'highlite.treesitter' --- @type highlite.Treesitter
-
 if vim.api.nvim_get_var('colors_name'):find '^highlite' then
-	Treesitter.add_directive.offset_from_start()
+	require('highlite.treesitter').add_directive.offset_from_start()
 end
