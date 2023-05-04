b1999ea3fa0c87a3bcfd98b2d952ac5349482e83
Iron-E <code.iron.e@gmail.com>
2023-05-04 17:55:58 -0400

    ref: normalize H1…H6 for all languages

--- after/queries/html/highlights.scm
+++ after/queries/html/highlights.scm
@@ -2,3 +2,34 @@
 
 ; doctype as preproc (i.e. shebang)
 (doctype) @preproc
+
+; headers
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h1"))
+	(text) @text.title.h1
+)
+
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h2"))
+	(text) @text.title.h2
+)
+
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h3"))
+	(text) @text.title.h3
+)
+
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h4"))
+	(text) @text.title.h4
+)
+
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h5"))
+	(text) @text.title.h5
+)
+
+(element
+	(start_tag (tag_name) @_tag (#eq? @_tag "h6"))
+	(text) @text.title.h6
+)
