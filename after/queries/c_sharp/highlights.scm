b9dd0dc8acedef358490e3855869046165572c32
Iron-E <code.iron.e@gmail.com>
2023-05-03 22:59:07 -0400

    ref(c_sharp): improve TS support

--- after/queries/c_sharp/highlights.scm
+++ after/queries/c_sharp/highlights.scm
@@ -1,10 +1,47 @@
 ;; extends
 
-; highlight accessors as operator-keywords
+; accessors as operator-keywords
 (accessor_declaration _ @keyword.operator)
 
-; highlight arrows as delimiters
+; arrows as delimiters
 "=>" @punctuation.delimiter
 
-; highlight interpolations with the dedicated interpolation group
+; conditionals
+(conditional_expression ["?" ":"] @conditional.ternary)
+
+; identifiers
+((identifier) @variable.builtin (#eq? @variable.builtin "_"))
+
+; interpolations with the dedicated interpolation group
 (interpolation ["{" "}"] @punctuation.special)
+
+; lambda
+(lambda_expression
+	parameters: (parameter_list . "(" @function ")" @function .)
+)
+
+; macro
+(region_directive (preproc_message) @text.title) @preproc
+(endregion_directive (preproc_message) @text.title) @preproc
+
+(
+	[
+		(region_directive)
+		(endregion_directive)
+	] @punctuation.delimiter
+	(#offset-from-start! @punctuation.delimiter 0 -1 0 1)
+)
+
+; namespaces
+(using_directive (identifier) @namespace)
+(qualified_name (identifier) @namespace
+	(#has-ancestor? @namespace namespace_declaration using_directive) ; NOTE: from nvim-treesitter
+)
+
+; nullable
+(conditional_access_expression "?" @punctuation.special)
+(nullable_type "?" @punctuation.special)
+
+; operators
+(binary_expression ["<" ">"] @operator)
+(binary_expression "??" @operator)
---
+++ ftplugin/cs/highlite.lua
@@ -0,0 +1,3 @@
+if vim.api.nvim_get_var('colors_name'):find '^highlite' then
+	require('highlite.treesitter').add_directive.offset_from_start()
+end
