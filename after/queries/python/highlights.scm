425a6a5b921c7486f990efadcf09c598e1ad18c2
Iron-E <code.iron.e@gmail.com>
2023-05-03 23:28:36 -0400

    feat(python): better TS support

---
+++ after/queries/python/highlights.scm
@@ -0,0 +1,22 @@
+;; extends
+
+; namespaces
+(aliased_import alias: (identifier) @namespace)
+(
+	[
+		(import_from_statement module_name: (dotted_name (identifier) @namespace))
+		(import_statement name:
+		[
+			(aliased_import name: (dotted_name (identifier) @namespace))
+			(dotted_name (identifier) @namespace)
+		])
+	]
+	(#lua-match? @namespace "^[a-z_0-9]+$")
+)
+
+; repeats
+(for_in_clause "in" @repeat)
+(for_statement "in" @repeat)
+
+; shebang
+((comment) @preproc (#lua-match? @preproc "^#!"))
