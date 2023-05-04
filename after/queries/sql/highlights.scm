42d6e2b75d281e9130baa2d38d6ca8eb2e838fd0
Iron-E <code.iron.e@gmail.com>
2023-05-03 23:39:40 -0400

    feat(sql): improve TS support

---
+++ after/queries/sql/highlights.scm
@@ -0,0 +1,25 @@
+;; extends
+
+; functions
+[
+	(keyword_array)
+	(keyword_brin)
+	(keyword_btree)
+	(keyword_count)
+	(keyword_float)
+	(keyword_gin)
+	(keyword_gist)
+	(keyword_group_concat)
+	(keyword_hash)
+	(keyword_spgist)
+] @function.builtin
+
+; operators
+[
+	(keyword_as)
+	(keyword_cast)
+] @keyword.operator
+
+; structures
+(field table_alias: (identifier) @structure)
+(relation table_alias: (identifier) @structure)
