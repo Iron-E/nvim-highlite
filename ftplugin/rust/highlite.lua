624fddaf856add7a4a9a40b33fad3bb3818f4fc8
Iron-E <code.iron.e@gmail.com>
2023-05-04 19:20:35 -0400

    fix(treesitter): parsers attach with invalid directives
    This is because ftplugins do not run for when parsers attach. I will be
    able to delay this when a `TreesitterAttach` event becomes available.

--- ftplugin/rust/highlite.lua
+++
@@ -1,5 +0,0 @@
-if vim.api.nvim_get_var('colors_name'):find '^highlite' then
-	local Treesitter = require 'highlite.treesitter' --- @type highlite.Treesitter
-	Treesitter.add_directive.offset_from_end()
-	Treesitter.add_directive.offset_from_start()
-end
--- lua/highlite/init.lua
+++ lua/highlite/init.lua
@@ -1,5 +1,69 @@
 local Table = require 'highlite.table' --- @type highlite.Table
 
+-- TODO: use `TreesitterAttach` when it is available {{{
+vim.api.nvim_create_autocmd('SourcePost', {
+	callback = function()
+		vim.treesitter.query.add_directive('offset-from-end!',
+			--- @param match {active: boolean, pattern: integer, [integer]: nil|TSNode}
+			--- @param predicate {[1]: string, [2]: integer, [3]: nil|integer, [4]: nil|integer, [5]: nil|integer, [6]: nil|integer}
+			--- @param metadata table
+			function(match, _, _, predicate, metadata)
+				local capture_id = predicate[2]
+				if not metadata[capture_id] then
+					metadata[capture_id] = {}
+				end
+
+				local range = metadata[capture_id].range or {match[capture_id]:range()}
+				local start_row_offset = predicate[3] or 0
+				local start_col_offset = predicate[4] or 0
+				local end_row_offset = predicate[5] or 0
+				local end_col_offset = predicate[6] or 0
+
+				range[1] = range[3] + start_row_offset
+				range[2] = range[4] + start_col_offset
+				range[3] = range[3] + end_row_offset
+				range[4] = range[4] + end_col_offset
+
+				-- If this produces an invalid range, we just skip it.
+				if range[1] < range[3] or (range[1] == range[3] and range[2] <= range[4]) then
+					metadata[capture_id].range = range
+				end
+			end,
+			true
+		)
+
+		vim.treesitter.query.add_directive('offset-from-start!',
+			--- @param match {active: boolean, pattern: integer, [integer]: nil|TSNode}
+			--- @param predicate {[1]: string, [2]: integer, [3]: nil|integer, [4]: nil|integer, [5]: nil|integer, [6]: nil|integer}
+			--- @param metadata table
+			function(match, _, _, predicate, metadata)
+				local capture_id = predicate[2]
+				if not metadata[capture_id] then
+					metadata[capture_id] = {}
+				end
+
+				local range = metadata[capture_id].range or {match[capture_id]:range()}
+				local start_row_offset = predicate[3] or 0
+				local start_col_offset = predicate[4] or 0
+				local end_row_offset = predicate[5] or 0
+				local end_col_offset = predicate[6] or 0
+
+				range[3] = range[1] + end_row_offset
+				range[4] = range[2] + end_col_offset
+				range[1] = range[1] + start_row_offset
+				range[2] = range[2] + start_col_offset
+
+				-- If this produces an invalid range, we just skip it.
+				if range[1] < range[3] or (range[1] == range[3] and range[2] <= range[4]) then
+					metadata[capture_id].range = range
+				end
+			end,
+			true
+		)
+	end,
+	once = true,
+})
+
 --- @class highlite.config
 --- @field generate highlite.groups.from_palette.opts options for highlight group generation
 --- @field terminal_palette boolean if `false`, skip setting the terminal colors.
