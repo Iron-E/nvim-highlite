d5654c3a8951f6c503cb8083a0e9dbfc35a5f59c
Iron-E <code.iron.e@gmail.com>
2023-05-04 23:39:12 -0400

    ref(markdown): improve link label highlighting

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -9,6 +9,10 @@
 ; punctuation
 ((list_marker_dot) @punctuation.delimiter (#offset-from-end! @punctuation.delimiter 0 -2 0 0))
 
+(link_reference_definition ":" @punctuation.delimiter)
+(link_reference_definition (link_label) @punctuation.bracket (#offset-from-end! @punctuation.bracket 0 -1 0 0))
+(link_reference_definition (link_label) @punctuation.bracket (#offset-from-start! @punctuation.bracket 0 0 0 1))
+
 ; BUG: doesn't work because of neovim/neovim#17099
 ; (pipe_table
 ; 	(pipe_table_header . "|" "|"* @punctuation.delimiter "|" .)
