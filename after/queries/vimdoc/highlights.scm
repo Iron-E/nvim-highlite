efba1524e6dd80ed30c2abfd17dcbc043f565f7c
Iron-E <code.iron.e@gmail.com>
2023-05-05 15:23:01 -0400

    feat(vimdoc): improve concealability

--- after/queries/vimdoc/highlights.scm
+++ after/queries/vimdoc/highlights.scm
@@ -1,3 +1,13 @@
 ;; extends
 
+; options
+(optionlink
+	"'" @conceal (#set! conceal "")
+	text: (word) @variable.builtin
+)
+
+; parameters
+(argument ["{" "}"] @conceal (#set! conceal ""))
+
+; special keys
 (keycode) @string.keycode
