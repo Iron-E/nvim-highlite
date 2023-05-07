6c79f2c0b4aba5e44fb3058fd88959772c3104ad
Iron-E <code.iron.e@gmail.com>
2023-05-06 21:01:53 -0400

    ref(toml): highlight tables consistently

--- after/queries/toml/highlights.scm
+++ after/queries/toml/highlights.scm
@@ -1,14 +1,14 @@
 ;; extends
 
-; show table names as structures
-(table .
-[
-	(bare_key) @structure
-	(dotted_key (bare_key) @structure)
-])
+; show tables as structures
+(dotted_key (bare_key) @structure)
 
-(table_array_element .
-[
-	(bare_key) @structure
-	(dotted_key (bare_key) @structure)
-])
+(pair (bare_key) @structure (inline_table))
+(pair
+	(dotted_key (bare_key) @field .)
+	(_) @_value
+	(#not-has-type? @_value inline_table)
+)
+
+(table (bare_key) @structure)
+(table_array_element (bare_key) @structure)
