62b503f1ce7e2dc725beac43a9bce526a5186dba
Iron-E <code.iron.e@gmail.com>
2023-05-15 14:10:07 -0400

    feat: support for official H1…H6 groups
    There wasn't originally any group for this, so I made my own. Somewhat
    unsurprisingly, a different name was chosen

--- after/queries/html/highlights.scm
+++ after/queries/html/highlights.scm
@@ -6,30 +6,30 @@
 ; headers
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h1"))
-	(text) @text.title.h1
+	(text) @text.title.1
 )
 
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h2"))
-	(text) @text.title.h2
+	(text) @text.title.2
 )
 
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h3"))
-	(text) @text.title.h3
+	(text) @text.title.3
 )
 
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h4"))
-	(text) @text.title.h4
+	(text) @text.title.4
 )
 
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h5"))
-	(text) @text.title.h5
+	(text) @text.title.5
 )
 
 (element
 	(start_tag (tag_name) @_tag (#eq? @_tag "h6"))
-	(text) @text.title.h6
+	(text) @text.title.6
 )
--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -23,11 +23,11 @@
 ; titles
 (atx_heading
 	[
-		((atx_h1_marker) heading_content: (inline) @text.title.h1)
-		((atx_h2_marker) heading_content: (inline) @text.title.h2)
-		((atx_h3_marker) heading_content: (inline) @text.title.h3)
-		((atx_h4_marker) heading_content: (inline) @text.title.h4)
-		((atx_h5_marker) heading_content: (inline) @text.title.h5)
-		((atx_h6_marker) heading_content: (inline) @text.title.h6)
-	]
+		(atx_h1_marker)
+		(atx_h2_marker)
+		(atx_h3_marker)
+		(atx_h4_marker)
+		(atx_h5_marker)
+		(atx_h6_marker)
+	] @punctuation.special
 )
