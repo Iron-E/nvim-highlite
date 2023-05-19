21cc97ea72b5271f9ec9779c765dcbdaa7ea6a83
Iron-E <code.iron.e@gmail.com>
2023-05-19 00:14:22 -0400

    fix(rust): LspTokenUpdate autocmd running on every buffer

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,9 +1,10 @@
 vim.api.nvim_create_autocmd('LspTokenUpdate', {
+	buffer = 0,
 	callback = function(event)
 		local bufnr = event.buf
 		local token = event.data.token
-		if token.type == 'operator' and
-			vim.api.nvim_buf_get_text(bufnr, token.line, token.start_col, token.line, token.end_col, {})[1] == '::'
+		if token.type == 'operator' and token.modifiers.attribute
+			and vim.api.nvim_buf_get_text(bufnr, token.line, token.start_col, token.line, token.end_col, {})[1] == ':'
 		then
 			vim.lsp.semantic_tokens.highlight_token(token, bufnr, event.data.client_id, '@punctuation.delimiter')
 		end
