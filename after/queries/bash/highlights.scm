291c729d67fa650b9bdf8beea2d4fb3972f8d317
Iron-E <code.iron.e@gmail.com>
2023-05-31 15:39:21 -0400

    highlight `in` (i.e. `for … in …; do; …; end`) like `for`

--- after/queries/bash/highlights.scm
+++ after/queries/bash/highlights.scm
@@ -4,5 +4,8 @@
 (command_substitution ["$(" ")"] @punctuation.special)
 (expansion ["${" "}"] @punctuation.special)
 
+; repeat
+(for_statement "in" @repeat)
+
 ; variables
 (variable_name) @variable
