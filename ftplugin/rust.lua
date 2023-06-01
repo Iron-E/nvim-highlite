939cab8aaea63dfb581593477863d31b8adac28f
Iron-E <code.iron.e@gmail.com>
2023-06-01 15:17:12 -0400

    fix(rust): `::` in attributes

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -4,9 +4,9 @@ vim.api.nvim_create_autocmd('LspTokenUpdate', {
 		local bufnr = event.buf
 		local token = event.data.token
 		if token.type == 'operator' and token.modifiers.attribute
-			and vim.api.nvim_buf_get_text(bufnr, token.line, token.start_col, token.line, token.end_col, {})[1] == ':'
+			and vim.api.nvim_buf_get_text(bufnr, token.line, token.start_col, token.line, token.end_col, {})[1] == '::'
 		then
-			vim.lsp.semantic_tokens.highlight_token(token, bufnr, event.data.client_id, '@punctuation.delimiter')
+			vim.lsp.semantic_tokens.highlight_token(token, bufnr, event.data.client_id, '@punctuation.delimiter.rust')
 		end
 	end,
 	group = vim.api.nvim_create_augroup('highlite-lsp', {clear = false})
