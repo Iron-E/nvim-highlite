3bcd15b23b70cbfde7b517ac7ad3b9b5f7970084
Iron-E <code.iron.e@gmail.com>
2023-12-04 12:37:14 -0500

    feat(python): show namespaces where possible

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,5 +1,11 @@
 ;; extends
 
+; Namespaces
+(attribute
+  object: (identifier) @namespace (#lua-match? @namespace "[_a-z]+")
+  attribute: (identifier) @_type (#lua-match? @_type "^_*[A-Z]+[a-z]+")
+)
+
 ; Privates
 ((identifier) @type (#lua-match? @type "^_+[A-Z]+[a-z]+"))
 ((identifier) @constant (#lua-match? @constant "^[A-Z_]+$"))
