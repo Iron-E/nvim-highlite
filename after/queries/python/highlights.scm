dbbc4fba9110fc49dee8cc46df7a27c8d05da8fa
Iron-E <code.iron.e@gmail.com>
2023-12-04 12:31:15 -0500

    fix(python): private types & constants highlighted as identifiers

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,27 +1,5 @@
 ;; extends
 
-; decorator
-(decorator "@" @punctuation.special
-	(#set! "priority" 101)
-)
-
-; namespaces
-(aliased_import alias: (identifier) @namespace)
-(
-	[
-		(import_from_statement module_name: (dotted_name (identifier) @namespace))
-		(import_statement name:
-		[
-			(aliased_import name: (dotted_name (identifier) @namespace))
-			(dotted_name (identifier) @namespace)
-		])
-	]
-	(#lua-match? @namespace "^[a-z_0-9]+$")
-)
-
-; repeats
-(for_in_clause "in" @repeat)
-(for_statement "in" @repeat)
-
-; shebang
-((comment) @preproc (#lua-match? @preproc "^#!"))
+; Privates
+((identifier) @type (#lua-match? @type "^_+[A-Z]+[a-z]+"))
+((identifier) @constant (#lua-match? @constant "^[A-Z_]+$"))
