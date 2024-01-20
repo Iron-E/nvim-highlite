60cb6860eaff8a70fc07a9406f2f43a9a97e33a1
Iron-E <code.iron.e@gmail.com>
2024-01-19 23:14:57 -0500

    ref(markdown_inline): improve bracket usage consistency

--- after/queries/markdown_inline/highlights.scm
+++ after/queries/markdown_inline/highlights.scm
@@ -1,3 +1,13 @@
 ;; extends
 
+; punctuation
 (inline_link ["[" "]" "(" ")"] @punctuation.bracket)
+(image
+	"!" @punctuation.special
+	["[" "]" "(" ")"] @punctuation.bracket
+)
+
+[
+	(code_span_delimiter)
+	(emphasis_delimiter)
+] @markup.environment
