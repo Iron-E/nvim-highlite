dea6dcb9418b3a854e7d6c61a8ac33beef10fa71
Iron-E <code.iron.e@gmail.com>
2024-02-16 14:47:59 -0500

    feat(queries): dockerfile support

---
+++ after/queries/dockerfile/highlights.scm
@@ -0,0 +1,37 @@
+;; extends
+
+; character highlighting
+
+[
+	"="
+] @operator
+
+[
+	":"
+	","
+	(line_continuation)
+] @punctuation.delimiter
+
+
+; paths
+(path) @string.special.path
+(image_name) @module
+
+; values
+
+(
+	[
+		(image_tag)
+		(unquoted_string)
+	] @string
+	(#set! "priority" 99)
+)
+
+; variables
+[
+	(param)
+	(mount_param_param)
+] @parameter
+
+(image_alias) @variable
+(image_tag) @variable.member
