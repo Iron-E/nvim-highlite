6c177613d5de2962c4d5b79d96894d77b7b55c31
Iron-E <code.iron.e@gmail.com>
2024-02-16 19:37:02 -0500

    feat(dockerfile): mount args

--- after/queries/dockerfile/highlights.scm
+++ after/queries/dockerfile/highlights.scm
@@ -27,6 +27,20 @@
 (path) @string.special.path
 (image_name) @module
 
+; params
+[
+	(param)
+	(mount_param_param)
+] @parameter
+
+([(param) (mount_param_param)] @variable (#match? @variable "^(--)?from\\=") (#offset! @variable 0 5 0 0))
+((mount_param_param) @string (#lua-match? @string "^id%=") (#offset! @string 0 3 0 0))
+((mount_param_param) @number (#lua-match? @number "^[gu]id%=") (#offset! @number 0 4 0 0))
+((mount_param_param) @number (#lua-match? @number "^mode%=") (#offset! @number 0 5 0 0))
+((mount_param_param) @type.qualifier (#lua-match? @type.qualifier "^sharing%=") (#offset! @type.qualifier 0 8 0 0))
+((mount_param_param) @string.special.path (#match? @string.special.path "^(source|target)\\=") (#offset! @string.special.path 0 7 0 0))
+((mount_param_param) @keyword.storage (#lua-match? @keyword.storage "^type%=") (#offset! @keyword.storage 0 5 0 0))
+
 ; values
 
 (
@@ -39,10 +53,6 @@
 )
 
 ; variables
-[
-	(param)
-	(mount_param_param)
-] @parameter
 
 (image_alias) @variable
 (image_tag) @variable.member
