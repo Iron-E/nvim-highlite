cb25108b42c0a7b42fbc24b661eac3174c12bb12
Iron-E <code.iron.e@gmail.com>
2024-02-20 12:10:06 -0500

    fix(init): `offset-from!` doesn't work with new `all = true` directive

--- lua/highlite/init.lua
+++ lua/highlite/init.lua
@@ -1,49 +1,53 @@
 local Table = require 'highlite.table' --- @type highlite.Table
 
 -- TODO: use `TreesitterAttach` when it is available {{{
-vim.api.nvim_create_autocmd('SourcePost', {
-	callback = function()
-		vim.treesitter.query.add_directive('offset-from!',
-			--- @param match {active: boolean, pattern: integer, [integer]: nil|TSNode}
-			--- @param predicate {[1]: string, [2]: 'end'|'start', [3]: integer, [4]: nil|integer, [5]: nil|integer, [6]: nil|integer, [7]: nil|integer}
-			--- @param metadata table
-			function(match, _, _, predicate, metadata)
-				local from = predicate[2]
-				local capture_id = predicate[3]
-				local start_row_offset = predicate[4] or 0
-				local start_col_offset = predicate[5] or 0
-				local end_row_offset = predicate[6] or 0
-				local end_col_offset = predicate[7] or 0
+vim.api.nvim_create_autocmd('SourcePost', {once = true, callback = function()
+	--- @param match {active: boolean, pattern: integer, [integer]: nil|TSNode}
+	--- @param predicate {[1]: string, [2]: 'end'|'start', [3]: integer, [4]: nil|integer, [5]: nil|integer, [6]: nil|integer, [7]: nil|integer}
+	--- @param metadata table
+	local function handler(match, _, _, predicate, metadata)
+		local from = predicate[2]
+		local capture_id = predicate[3]
+		local start_row_offset = predicate[4] or 0
+		local start_col_offset = predicate[5] or 0
+		local end_row_offset = predicate[6] or 0
+		local end_col_offset = predicate[7] or 0
 
-				if not metadata[capture_id] then
-					metadata[capture_id] = {}
-				end
+		local nodes = match[capture_id]
+		if nodes == nil then
+			return
+		end
 
-				local range = metadata[capture_id].range or {match[capture_id]:range()}
+		--- NOTE: `nodes` is a table on nvim 0.10+
+		local node = type(nodes) == 'table' and nodes[1] or nodes
 
-				if from == 'end' then
-					range[1] = range[3] + start_row_offset
-					range[2] = range[4] + start_col_offset
-					range[3] = range[3] + end_row_offset
-					range[4] = range[4] + end_col_offset
-				else
-					-- NOTE: have to assign `3` & `4` because assigning `1` & `2` alters the relative offset
-					range[3] = range[1] + end_row_offset
-					range[4] = range[2] + end_col_offset
-					range[1] = range[1] + start_row_offset
-					range[2] = range[2] + start_col_offset
-				end
+		if not metadata[capture_id] then
+			metadata[capture_id] = {}
+		end
 
-				-- If this produces an invalid range, we just skip it.
-				if range[1] < range[3] or (range[1] == range[3] and range[2] <= range[4]) then
-					metadata[capture_id].range = range
-				end
-			end,
-			true
-		)
-	end,
-	once = true,
-})
+		local range = metadata[capture_id].range or { node:range() }
+
+		if from == 'end' then
+			range[1] = range[3] + start_row_offset
+			range[2] = range[4] + start_col_offset
+			range[3] = range[3] + end_row_offset
+			range[4] = range[4] + end_col_offset
+		else
+			-- NOTE: have to assign `3` & `4` because assigning `1` & `2` alters the relative offset
+			range[3] = range[1] + end_row_offset
+			range[4] = range[2] + end_col_offset
+			range[1] = range[1] + start_row_offset
+			range[2] = range[2] + start_col_offset
+		end
+
+		-- If this produces an invalid range, we just skip it.
+		if range[1] < range[3] or (range[1] == range[3] and range[2] <= range[4]) then
+			metadata[capture_id].range = range
+		end
+	end
+
+	vim.treesitter.query.add_directive('offset-from!', handler, {force = true, all = true})
+end})
 
 --- @class highlite.config
 --- @field generate highlite.groups.from_palette.opts options for highlight group generation
