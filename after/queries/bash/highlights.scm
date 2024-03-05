385ed867a4cd8c38255f328c32e3ec44c71d94c0
Iron-E <code.iron.e@gmail.com>
2024-03-05 11:47:30 -0500

    ref(bash): align substitution highlighting with fish

--- after/queries/bash/highlights.scm
+++ after/queries/bash/highlights.scm
@@ -1,8 +1,12 @@
 ;; extends
 
 ; interpolation
-(command_substitution ["`" "$(" ")"] @punctuation.special)
-(expansion ["${" "}"] @punctuation.special)
+(command_substitution "`" @punctuation.special)
+
+(command_substitution "$(" @punctuation.special (#offset! @punctuation.special 0 0 0 -1))
+(expansion "${" @punctuation.bracket (#offset! @punctuation.bracket 0 1 0 0))
+(expansion "${" @punctuation.special (#offset! @punctuation.special 0 0 0 -1))
+(expansion "}" @punctuation.bracket)
 
 ; repeat
 (for_statement "in" @keyword.repeat)
