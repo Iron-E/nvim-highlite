f4c42eac00e5fe5e57d10654aa4654b2ef8692c4
Iron-E <code.iron.e@gmail.com>
2024-03-05 21:20:43 -0500

    fix(markdown): header marking highlighting

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,6 +1,7 @@
 ;; extends
 
 ; punctuation
+
 ((list_marker_dot) @punctuation.delimiter (#offset-from! "end" @punctuation.delimiter 0 -2 0 0))
 
 (link_reference_definition
@@ -9,4 +10,18 @@
 )
 
 ; text environments
+
 (fenced_code_block (info_string) @markup.environment.name (#set! priority 101))
+
+; titles
+
+(atx_heading
+	[
+		(atx_h1_marker)
+		(atx_h2_marker)
+		(atx_h3_marker)
+		(atx_h4_marker)
+		(atx_h5_marker)
+		(atx_h6_marker)
+	] @punctuation.special
+)
