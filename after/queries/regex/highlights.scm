739466dbdef58add1c0dd7fd2d01d882a379d83c
Iron-E <code.iron.e@gmail.com>
2024-03-17 19:12:13 -0400

    feat(regex): range highlighting

--- after/queries/regex/highlights.scm
+++ after/queries/regex/highlights.scm
@@ -7,6 +7,48 @@
 	(end_assertion)
 ] @punctuation.special
 
+(character_class
+	(class_character) @_range_start
+	.
+	(class_character) @operator
+	.
+	(class_character) @_range_end
+
+	(#lua-match? @_range_start "[0-8]")
+	(#lua-match? @_range_end "[1-9]")
+	(#eq? @operator "-")
+
+	(#set! "priority" 101)
+)
+
+(character_class
+	(class_character) @_range_start
+	.
+	(class_character) @operator
+	.
+	(class_character) @_range_end
+
+	(#lua-match? @_range_start "[a-y]")
+	(#lua-match? @_range_end "[b-z]")
+	(#eq? @operator "-")
+
+	(#set! "priority" 101)
+)
+
+(character_class
+	(class_character) @_range_start
+	.
+	(class_character) @operator
+	.
+	(class_character) @_range_end
+
+	(#lua-match? @_range_start "[A-Y]")
+	(#lua-match? @_range_end "[B-Z]")
+	(#eq? @operator "-")
+
+	(#set! "priority" 101)
+)
+
 ; qualifiers
 
 (
