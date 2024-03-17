fc29abd9c720830b2883b55e48163af8a66bab30
Iron-E <code.iron.e@gmail.com>
2024-03-17 19:22:07 -0400

    feat(nix): imports highlighted as modules

--- after/queries/nix/highlights.scm
+++ after/queries/nix/highlights.scm
@@ -1,5 +1,30 @@
 ;; extends
 
+; modules
+
+(apply_expression ; `import` args
+	function: (variable_expression
+		name: (identifier) @_fn
+		(#eq? @_fn "import")
+	)
+
+	argument: [
+		(path_expression (path_fragment) @module)
+		(spath_expression) @module
+	]
+)
+
+(binding ; imports in nixos and home-manager modules
+	attrpath: (attrpath
+		attr: (identifier) @_key
+		(#eq? @_key "imports")
+	)
+
+	expression: (list_expression
+		element: (path_expression (path_fragment) @module)
+	)
+)
+
 ; punctuation
 
 (binding "=" @operator)
