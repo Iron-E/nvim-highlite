2d35a04be004e11a1e90a9de98fd2e51847f8f53
Iron-E <code.iron.e@gmail.com>
2024-04-30 19:05:01 -0400

    chore(bash): remove upstreamed queries

--- after/queries/bash/highlights.scm
+++ after/queries/bash/highlights.scm
@@ -3,10 +3,5 @@
 ; interpolation
 (command_substitution "`" @punctuation.special)
 
-(command_substitution "$(" @punctuation.special (#offset! @punctuation.special 0 0 0 -1))
-(expansion "${" @punctuation.bracket (#offset! @punctuation.bracket 0 1 0 0))
-(expansion "${" @punctuation.special (#offset! @punctuation.special 0 0 0 -1))
-(expansion "}" @punctuation.bracket)
-
 ; repeat
 (for_statement "in" @keyword.repeat)
