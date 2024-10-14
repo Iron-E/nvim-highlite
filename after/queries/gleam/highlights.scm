bcfeee8f78dd5a98cad5b96245baa8f37e61a5c8
Iron-E <code.iron.e@gmail.com>
2024-10-14 19:40:02 -0400

    feat(gleam): highlight attributes

--- after/queries/gleam/highlights.scm
+++ after/queries/gleam/highlights.scm
@@ -1,11 +1,20 @@
 ;; extends
 
-; punctuation
+; modifiers
 
-(module "/" @punctuation.delimiter)
+(attribute
+	.
+	"@" @punctuation.special
+	.
+	name: (identifier) @attribute
+)
 
 ; operator
 
 (case_clause_patterns "|" @operator)
 (discard) @variable.builtin
 (use "<-" @operator)
+
+; punctuation
+
+(module "/" @punctuation.delimiter)
