9097dfdab09c0610c5057a0161f68d9e9a187657
Iron-E <code.iron.e@gmail.com>
2025-06-03 10:05:16 -0400

    ref(gotmpl): use standardized text interpolation highlighting
    Go template syntax was using plain bracket highlights, which made it
    slightly unclear compared to other languages where interpolation was
    taking place.

--- after/queries/gotmpl/highlights.scm
+++ after/queries/gotmpl/highlights.scm
@@ -6,3 +6,30 @@
 	"$" @punctuation.special
 	(#set! "priority" 110)
 )
+
+(function_call
+	function: (identifier) @operator
+	(#any-of? @operator "and" "eq" "gt" "lt" "ne" "not" "or")
+	(#set! "priority" 111)
+)
+
+(function_call
+	function: (identifier) @keyword.import
+	(#eq? @keyword.import "include")
+	(#set! "priority" 111)
+)
+
+(
+	[
+		"-}}"
+		"{{"
+		"{{-"
+		"}}"
+	] @punctuation.special
+	(#set! "priority" 111)
+)
+
+(variable
+	"$" @constant.builtin
+	(#set! "priority" 111)
+)
