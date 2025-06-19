ca7eda8016fc5a609d5fc7af1cd1d56d3e3df86e
Iron-E <code.iron.e@gmail.com>
2025-06-19 17:25:14 -0400

    feat(json5): queries
    The highlighting for json5 was not at all like json/jsonc.
    While I can't make it totally match (since strings are not separate from
    their content), I can make it closer.

---
+++ after/queries/json5/highlights.scm
@@ -0,0 +1,29 @@
+;; extends
+
+; punctuation
+
+[
+	"{"
+	"["
+	"]"
+	"}"
+] @punctuation.bracket
+
+[
+	":"
+	","
+] @punctuation.delimiter
+
+
+; identifiers
+
+(member
+	name: (string) @variable.member
+	(#not-eq? @variable.member "")
+	(#offset! @variable.member 0 1 0 -1)
+	(#set! "priority" 101)
+)
+
+; keywords
+
+(null) @constant.builtin
