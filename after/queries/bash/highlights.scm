80df8ab9ef2a42e3616dedd010a8a49f3627bff0
Iron-E <code.iron.e@gmail.com>
2025-07-19 18:03:42 -0400

    feat(bash): associative array highlighting

--- after/queries/bash/highlights.scm
+++ after/queries/bash/highlights.scm
@@ -5,3 +5,22 @@
 
 ; repeat
 (for_statement "in" @keyword.repeat)
+
+; associative arrays
+(array
+	(concatenation
+		.
+		(word) @punctuation.bracket @_left_bracket
+		(#eq? @_left_bracket "[")
+		.
+		(_) ; inner text
+		.
+		(word) @punctuation.bracket @_right_bracket
+		(#eq? @_right_bracket "]")
+		.
+		(word) @operator
+		(#lua-match? @operator "^%=") ; can't be eq, since if we have ['foo']=4 then (word) matches =4
+		(#offset-from! "start" @operator 0 0 0 1)
+		.
+	)
+)
