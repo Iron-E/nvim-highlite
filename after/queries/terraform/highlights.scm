5826d24c9e63dddc6bebcda33de2da00e545ec89
Iron-E <code.iron.e@gmail.com>
2025-11-13 14:54:47 -0500

    feat(terraform/queries): highlight splat expressions

--- after/queries/terraform/highlights.scm
+++ after/queries/terraform/highlights.scm
@@ -141,3 +141,13 @@
 (for_intro ":" @punctuation.delimiter)
 
 ("=>" @punctuation.delimiter)
+
+(splat
+	(full_splat "[*]" @character.special)
+	(#offset! @character.special 0 1 0 -1)
+)
+
+(splat
+	(attr_splat ".*" @character.special)
+	(#offset! @character.special 0 1 0 0)
+)
