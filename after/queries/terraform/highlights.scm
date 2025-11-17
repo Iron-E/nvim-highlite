0f115b65099e4866785939c43cf31bc2012ec15e
Iron-E <code.iron.e@gmail.com>
2025-11-17 18:43:50 -0500

    feat(queries): extract HCL syntax from terraform to hcl
    This makes hcl highlight consistently with terraform,
    except for terraform-specific syntax

--- after/queries/terraform/highlights.scm
+++ after/queries/terraform/highlights.scm
@@ -125,29 +125,3 @@
 		"var"
 	)
 )
-
-[
-	(null_lit)
-] @constant.builtin
-
-; punctuation
-
-(conditional [ "?" ":" ] @keyword.conditional.ternary)
-
-("=" @operator
-	(#has-parent? @operator attribute object_elem)
-)
-
-(for_intro ":" @punctuation.delimiter)
-
-("=>" @punctuation.delimiter)
-
-(splat
-	(full_splat "[*]" @character.special)
-	(#offset! @character.special 0 1 0 -1)
-)
-
-(splat
-	(attr_splat ".*" @character.special)
-	(#offset! @character.special 0 1 0 0)
-)
