22a9c3a1299f1b56ffd3e2624e5eda04f643b89f
Iron-E <code.iron.e@gmail.com>
2025-12-29 16:04:05 -0500

    fix(c/highlights): invalid highlighting of constant imports

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -23,6 +23,7 @@
 	(#offset-from! "start" @punctuation.special 0 0 0 1)
 )
 
-(preproc_include path: _ @module (#offset! @module 0 1 0 -1))
+(preproc_include path: (string_literal (string_content) @module))
+
 ((system_lib_string) @punctuation.bracket (#offset-from! "start" @punctuation.bracket 0 0 0 1))
 ((system_lib_string) @punctuation.bracket (#offset-from! "end" @punctuation.bracket 0 -1 0 0))
