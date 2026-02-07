ab82c5dbff0eaae7bbf2b790f4135ba814db5fbc
Iron-E <code.iron.e@gmail.com>
2026-02-07 14:50:24 -0500

    feat(export): nushell support
    Not totally sure where the right place for the default
    generation should be, but I think the module path
    is probably better than some random place in the
    default config dir.

--- lua/highlite/export/init.lua
+++ lua/highlite/export/init.lua
@@ -1,26 +1,26 @@
-local Fs = require 'highlite.fs' --- @type highlite.Fs
+local Fs = require("highlite.fs") --- @type highlite.Fs
 
 --- @param format string
 --- @return highlite.export.format
 local function wrap(format)
 	return function(...)
 		--- @type highlite.export.format.module
-		local export = require('highlite.export.' .. format)
+		local export = require("highlite.export." .. format)
 		local file, content, opts = export(...)
 		Fs.write(file, content, opts)
 	end
 end
 
 --- @class highlite.Export
-local Export =
-{
-	bat = wrap 'bat',
-	fish = wrap 'fish',
-	ghostty = wrap 'ghostty',
-	nvim = wrap 'native.lua',
-	ripgrep = wrap 'ripgrep',
-	vim = wrap 'native.vim',
-	wezterm = wrap 'wezterm',
+local Export = {
+	bat = wrap("bat"),
+	fish = wrap("fish"),
+	ghostty = wrap("ghostty"),
+	nushell = wrap("nushell"),
+	nvim = wrap("native.lua"),
+	ripgrep = wrap("ripgrep"),
+	vim = wrap("native.vim"),
+	wezterm = wrap("wezterm"),
 }
 
 return Export
