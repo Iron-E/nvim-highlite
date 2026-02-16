d5498bb387cffdd199e733994ab7beb39ddbf4c9
Iron-E <code.iron.e@gmail.com>
2026-02-16 13:25:18 -0500

    feat(import): automatically derive future groups for imported palettes

--- lua/highlite/import/init.lua
+++ lua/highlite/import/init.lua
@@ -84,7 +84,7 @@ local function get(bg, get_terminal)
 		}
 	end
 
-	return palette, terminal_palette
+	return require("highlite.color.palette")._derive(palette), terminal_palette
 end
 
 return get
