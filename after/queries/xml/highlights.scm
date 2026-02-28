8bd74bc2a62fb79ba0a8e83ace7744c3a328b0ce
Iron-E <code.iron.e@gmail.com>
2026-02-28 18:52:29 -0500

    docs(xml): call out group which should be removed in the future

--- after/queries/xml/highlights.scm
+++ after/queries/xml/highlights.scm
@@ -1,6 +1,10 @@
 ;; extends
 
+; TODO: remove this capture in the future.
+; the @text group doesn't exist anymore, but it did when this was written.
+; some people might be using this still.
 (CharData) @text
+
 ((CharData) @boolean (#any-of? @boolean "true" "false"))
 ((CharData) @number (#lua-match? @number "^%d+$"))
 ((CharData) @number.float (#lua-match? @number.float "^%d+.%d*$"))
