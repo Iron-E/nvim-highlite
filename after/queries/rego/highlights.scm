48c9afaaa33113c9a96d24de95747a8dcb93955a
Iron-E <code.iron.e@gmail.com>
2026-09-05 13:32:26 -0400

    feat(rego): highlight improvements

---
+++ after/queries/rego/highlights.scm
@@ -0,0 +1,85 @@
+;; extends
+
+; punctuation
+
+[
+  (assignment)
+  "-"
+] @operator
+
+("|" @punctuation.delimiter
+ (#has-parent? @punctuation.delimiter object_compr array_compr set_compr))
+
+[
+  ":"
+  ","
+  "."
+] @punctuation.delimiter
+
+([
+   "$\""
+   "$`"
+ ] @punctuation.special
+ (#offset! @punctuation.special 0 0 0 -1))
+
+(interpolation [ "{" "}" ] @punctuation.special)
+
+; identifiers
+
+(empty_set
+  "set(" @type.builtin @punctuation.delimiter
+  ")" @punctuation.delimiter
+  (#offset! @type.builtin 0 0 0 -1)
+  (#offset! @punctuation.delimiter 0 3 0 0))
+
+(scalar "null" @constant.builtin
+        (#eq? @constant.builtin "null"))
+
+((var) @variable.builtin
+       (#any-of? @variable.builtin "_" "data" "input"))
+
+(ref_arg_brack "_" @variable.builtin)
+
+((import)
+ .
+ (ref
+   (var) @_head @module.builtin
+   (ref_arg (ref_arg_dot (var) @module.builtin))*)
+
+ (#any-of? @_head "rego" "future"))
+
+((import) . (ref) . (as) @keyword.import)
+
+
+(rule_head
+  . (var) @variable
+  (#set! priority 105))
+
+(rule_head
+  . (var) @function
+  (rule_args)
+  (#set! priority 105))
+
+; keywords
+
+([
+  (some)
+  (every)
+ ] @keyword.repeat
+ (in) @keyword.repeat)
+
+[
+  (and)
+  (contains)
+  (not)
+  (or)
+] @keyword.operator
+
+[
+  (if)
+  (else)
+] @keyword.conditional
+
+(with_modifier
+  (with) @keyword.modifier
+  (as) @keyword.operator)
